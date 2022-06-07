3faf924404030bdd750bb47595160b0b0e50c7a6
Deyvison Tavares <deyvisontav@gmail.com>
2022-06-07 18:06:44 -0300

    responsividade mobi

--- src/components/Numeros.jsx
+++ src/components/Numeros.jsx
@@ -1,6 +1,6 @@
 export function Numeros({numeros}) {
   return (
-    <div className=" w-16 h-16 flex justify-center items-center rounded-full bg-black text-white text-xl">
+    <div className="w-12 h-12 md:w-16 md:h-16 flex justify-center items-center rounded-full bg-black text-white md:text-xl text-base " >
          
           {numeros}
     </div>
--- src/components/Sorteador.jsx
+++ src/components/Sorteador.jsx
@@ -12,9 +12,9 @@ export function Sorteador() {
      
     
       
-    <div className='w-screen h-screen flex flex-col flex-center justify-center items-center'>
-      <div className='border-0 border-zinc-500 shadow-2xl shadow-black rounded-xl p-28'>
-         <h1 className="font-bold text-5xl text-center mb-10 ">Mega Sena</h1>  
+    <div className='w-2/4 h-screen m-auto md:w-screen md:h-screen flex flex-col flex-center justify-center items-center'>
+      <div className='border-0 border-zinc-500 shadow-2xl shadow-black rounded-xl md:p-28 p-4'>
+         <h1 className="font-bold md:text-5xl text-xl text-center mb-10 ">Mega Sena</h1>  
 
   
            <div className='flex flex-center justify-center space-x-4 '>
@@ -24,7 +24,7 @@ export function Sorteador() {
            </div>
            <div className='mt-10 flex flex-center justify-center'>
              <button
-             className=' text-lg bg-black hover:bg-gray-900 transition duration-150 ease-out hover:ease-in px-10 py-3 rounded-2xl text-white  '
+             className=' md:text-lg text-sm bg-black hover:bg-gray-900 transition duration-150 ease-out hover:ease-in md:px-10 md:py-3 px-9 py-3 rounded-2xl text-white  '
              onClick={() => setNumeros(mega())} >sortear</button>
            </div>
        </div>
